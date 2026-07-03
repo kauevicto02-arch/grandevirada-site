ecd7cdf248a391d6ccffe687014ab05e5e903455
Kaue Victor <kauevicto02@gmail.com>
2026-07-03 15:20:26 -0300

    Corrige erro do pixel

--- gta6-launchpad-prototype-main/src/components/LandingPage.tsx
+++ gta6-launchpad-prototype-main/src/components/LandingPage.tsx
@@ -32,19 +32,10 @@ useEffect(() => {
   script.src = "https://connect.facebook.net/en_US/fbevents.js";
   document.head.appendChild(script);
 
-  (window as any).fbq =
-  (window as any).fbq ||
-  function () {
-    ((window as any).fbq.q = (window as any).fbq.q || []).push(arguments);
-  };
-
-(window as any).fbq.loaded = true;
-(window as any).fbq.version = "2.0";
-(window as any).fbq.q = [];
-
+  (window as any).fbq = (window as any).fbq || function () {};
   (window as any).fbq("init", "8340091136102161");
   (window as any).fbq("track", "PageView");
-}, []);  
+}, []);
   return (
     <div className="landing-page">
       <style>{`
