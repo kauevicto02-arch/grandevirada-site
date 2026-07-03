d8aa47ece7ca57f82bee0362a5582bf27b242e65
Kaue Victor <kauevicto02@gmail.com>
2026-07-03 15:57:50 -0300

    Instala pixel no head

--- gta6-launchpad-prototype-main/src/routes/__root.tsx
+++ gta6-launchpad-prototype-main/src/routes/__root.tsx
@@ -107,10 +107,27 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <HeadContent />
+  <HeadContent />
 
-        
-      </head>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+
+        fbq('init', '8340091136102161');
+        fbq('track', 'PageView');
+      `,
+    }}
+  />
+
+</head>
 
       <body>
         {children}
@@ -122,16 +139,6 @@ function RootShell({ children }: { children: ReactNode }) {
 
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
-
- function RootComponent() {
-  const { queryClient } = Route.useRouteContext();
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Outlet />
-    </QueryClientProvider>
-  );
-}
 
   return (
     <QueryClientProvider client={queryClient}>
